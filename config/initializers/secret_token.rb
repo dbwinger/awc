2dd57900c7b07a118c4c19d39d7633fdd6692a36
Daryl Winger <dbwinger@gmail.com>
2013-08-14 13:37:40 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Awc::Application.config.secret_token = 'fa0df85587862aeadfe3ec65bc11cf43be3d5cffc046a50ebbc3e944d4b47e62bc9630370e0cdb7866fddf43b1d55279320079e47bc4c5a2fef53b4c86816e3f'
