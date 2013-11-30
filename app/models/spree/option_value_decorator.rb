f01f33a368f9ba88e800e4a0ac7e1d2aea02feee
Daryl Winger <dbwinger@gmail.com>
2013-11-30 12:28:04 -0500

    fix issue with variant pricing

--- app/models/spree/option_value_decorator.rb
+++ app/models/spree/option_value_decorator.rb
@@ -1,4 +1,11 @@
 Spree::OptionValue.class_eval do
   attr_accessible :name, :presentation, :option_values_attributes, :weight, :price_modifier
+
+  def product_variant_with_default_weight_unless_weight_type product_id
+    # If not weight type, use the default so price changes are accurate.
+    v = variants.where(:product_id => product_id)
+    v = v.where(:weight => 16) if option_type.name.downcase != "weight"
+    v.first
+  end
 end
 
