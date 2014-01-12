796fb25c496aed946d8452e2e036f58bcc93e500
Daryl Winger <dbwinger@gmail.com>
2014-01-11 21:22:15 -0500

    refactor/fix weight dropdown pricing

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,12 @@
+module ApplicationHelper
+  def option_type_options_for_select option_type
+    option_type
+    dropdown_options = option_type.option_values.map do |ov|
+      # Get a variant for the product with that option_value with default other options
+      variant = ov.product_variant_with_default_other_options(@product.id, option_type.name)
+      [ "#{ov.presentation} #{variant_price variant}", ov.id ] if variant
+    end
+    options_for_select(dropdown_options)
+  end
+end
+
--- app/models/spree/option_value_decorator.rb
+++ app/models/spree/option_value_decorator.rb
@@ -1,10 +1,14 @@
 Spree::OptionValue.class_eval do
   attr_accessible :name, :presentation, :option_values_attributes, :weight, :price_modifier
 
-  def product_variant_with_default_weight_unless_weight_type product_id
-    # If not weight type, use the default so price changes are accurate.
-    v = variants.where(:product_id => product_id)
-    v = v.where(:weight => 16) if option_type.name.downcase != "weight"
+  def product_variant_with_default_other_options product_id, option_type_name
+    # If not weight type, use the default weight so price changes are accurate.
+    if option_type_name.downcase == 'grind'
+      v = variants.where(:product_id => product_id, :weight => 16)
+    # If weight dropdown, use variant with whole bean so it doesn't add to the price
+    elsif option_type_name.downcase == 'weight'
+      v = Spree::Variant.where(:product_id => product_id).where("EXISTS (SELECT * FROM spree_option_values_variants WHERE variant_id = spree_variants.id AND option_value_id = #{self.id}) AND EXISTS (SELECT * FROM spree_option_values_variants WHERE variant_id = spree_variants.id AND option_value_id = (SELECT id FROM spree_option_values WHERE name ILIKE 'whole bean'))")
+    end
     v.first
   end
 end
