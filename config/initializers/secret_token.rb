b46e26e064d0d26ead14f19270ab4cc92edebf8c
dbwinger <dbwinger@gmail.com>
2014-04-10 17:36:30 -0400

    change secret token to log everyone out for HeartBleed

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Awc2::Application.config.secret_token = 'e958abd5cc9cc0b4a0a26b7d12bf09ee43dee11c1c88c920b696ea16c2fba49b9dd8d7cd2d93d736865e2a07a10445485499331c895e7e2c24c71afb7ffee914'
+seAwc2::Application.config.secret_token = 'e958abd5cc9cc0b4a0a26b7d12bf09ee43dee11c1c88c920b696ea16c2fba49b9dd8d7cd2d93d736865e2a07a10445485499331c895e7e2c24c71afb7ffee912'
