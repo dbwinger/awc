458d088e44e9885bee43b79806e67dc89faa1bf5
dbwinger <dbwinger@gmail.com>
2015-02-13 18:23:13 -0500

    sentry timeout

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -2,5 +2,7 @@ require 'raven'
 
 Raven.configure do |config|
   config.dsn = 'https://804e0ad2e48347c48a31ecb007843dc6:0f251cbfd74b410e940056165bfe204c@app.getsentry.com/37922'
+  config.timeout = 10
+  config.open_timeout = 10
 end
 
